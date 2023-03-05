0ebc01319f46b75eb29cf4c6aa4f0df605560c16
Zachary Cline <zachcline@gmail.com>
2023-03-05 16:13:09 -0500

    Remove non-API endpoints

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import os
 import tempfile
 from zipfile import ZipFile
 
-from flask import Flask, render_template, request, redirect, url_for, \
-    make_response, abort
+from flask import Flask, request
 import joblib
 import pandas as pd
 
@@ -29,59 +28,6 @@ object_model = joblib.load('object_model.joblib')
 
 
 app = Flask(__name__)
-
-
-@app.get("/")
-def landing_page():
-    return render_template("landing.html")
-
-
-@app.get("/text")
-def text_page():
-    return render_template("text.html")
-
-
-@app.get("/file")
-def file_page():
-    return render_template("file.html")
-
-
-@app.post("/text")
-def text_request():
-    subject = request.form.get("subject")
-    body = request.form.get("body")
-    email_df = pd.DataFrame({'subject': [subject,],'body': [body,]})
-    response_df = handle_request(email_df, 'text', return_prob=True)
-    resp = make_response(redirect(url_for('results')))
-    resp.set_cookie('spam_prob', 
-                    f"{100 * response_df.loc[0,'spam_probability']:.3f}")
-    resp.set_cookie('ham_prob', 
-                    f"{100 * (1 - response_df.loc[0,'spam_probability']):.3f}")
-    return resp
-
-
-@app.post("/file")
-def file_request():
-    file = request.files['file']
-    if file and allowed_file(file.filename):
-        email_obj = email.message_from_binary_file(file, policy=default)
-        response_df = handle_request(email_obj, "file", return_prob=True)
-    else:
-        abort(400)
-    resp = make_response(redirect(url_for('results')))
-    resp.set_cookie('spam_prob', 
-                    f"{100 * response_df.loc[0,'spam_probability']:.3f}")
-    resp.set_cookie('ham_prob', 
-                    f"{100 * (1 - response_df.loc[0,'spam_probability']):.3f}")
-    return resp
-
-
-@app.get("/result")
-def results():
-    spam_prob = request.cookies['spam_prob']
-    ham_prob = request.cookies['ham_prob']
-    return render_template("results.html", spam_prob=spam_prob, 
-                           ham_prob=ham_prob)
 
 
 @app.post("/api/predict/text")
