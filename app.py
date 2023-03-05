14a05784a67f6feaac9fdb33cdab219d42147c15
Zachary Cline <zachcline@gmail.com>
2023-03-05 17:55:26 -0500

    Refactor/edit request handling; Fixes #1; Fixes #2
    - Move functionality for creating requests to individual
      methods. The handle_request function seemed unnecessary.
    - Change the archive endpoint to return a single array rather
      than an array of arrays.
    - Add a "file" field to the JSON objects in the response to
      the archive endpoint.
    - Add 400 error handling for commonly expected mistakes in
      API requests.
    - Use a context manager for the temporary directory in the
      archive request handling, so that the temporary directory is
      deleted.
    - Use pathlib Path's .suffix attribute for checking allowed
      extensions of email and archive files. No need for extra
      function definitions.

--- app.py
+++ app.py
@@ -1,103 +1,98 @@
 import email
 from email.policy import default
-import os
+from pathlib import Path
 import tempfile
 from zipfile import ZipFile
 
-from flask import Flask, request
+from flask import Flask, request, abort, jsonify
 import joblib
 import pandas as pd
 
 
-ALLOWED_EXTENSIONS = {'eml'}
-ALLOWED_ARCHIVES = {'zip'}
+ALLOWED_EXTENSIONS = {'.eml'}
+ALLOWED_ARCHIVES = {'.zip'}
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.')[-1].lower() in ALLOWED_EXTENSIONS
-
-
-def allowed_archive(filename):
-    return '.' in filename and \
-        filename.rsplit('.')[-1].lower() in ALLOWED_ARCHIVES
-
-
-text_model = joblib.load('text_model.joblib')
-object_model = joblib.load('object_model.joblib')
+TEXT_MODEL = joblib.load('text_model.joblib')
+OBJECT_MODEL = joblib.load('object_model.joblib')
 
 
 app = Flask(__name__)
 
 
+@app.errorhandler(400)
+def bad_request(e):
+    return jsonify(error=str(e)), 400
+
+
 @app.post("/api/predict/text")
 def api_predict_text():
     input = request.json
-    email_df = pd.DataFrame(input['instances'])
-    rp = input.setdefault('return_prob', True)
-    ri = input.setdefault('return_inputs', False)
-    response_df = handle_request(email_df,'text', rp, ri)
+    if 'instances' not in input:
+        abort(400, description="Request must include an array with key "
+                               "'instances'")
+    try:
+        email_df = pd.DataFrame(input['instances'])[["subject", "body"]]
+    except (ValueError, KeyError):
+        abort(400, description="Key 'instances' in request must be an array "
+                               "of JSON objects with keys 'subject' and "
+                               "'body'")
+    response_df = pd.DataFrame(
+        {"prediction": TEXT_MODEL.predict(email_df).astype(bool)})
+    if input.setdefault('return_prob', True):
+        response_df['spam_probability'] = \
+            TEXT_MODEL.predict_proba(email_df)[:,1]
+    if input.setdefault('return_inputs', False):
+        response_df['subject_head'] = (email_df['subject'].str.slice(stop=100))
+        response_df['body_head'] = email_df['body'].str.slice(stop=100)
     return response_df.to_dict(orient='records')
 
 
 @app.post("/api/predict/file")
 def api_predict_file():
-    file = request.files['file']
-    if file and allowed_file(file.filename):
+    try:
+        file = request.files['file']
+    except KeyError:
+        abort(400, description="A file named 'file' was not found in the "
+                               "request")
+    if file and Path(file.filename).suffix in ALLOWED_EXTENSIONS:
         email_obj = email.message_from_binary_file(file, policy=default)
-    response_df = handle_request(email_obj,'file')
+    else:
+        abort(400, description="'file' must be one of the following file "
+                                f"types: {', '.join(ALLOWED_EXTENSIONS)}")
+    response_df = pd.DataFrame({
+            "prediction": OBJECT_MODEL.predict([email_obj,]).astype(bool),
+            "spam_probability": OBJECT_MODEL.predict_proba([email_obj,])[:,1]
+        })
     return response_df.to_dict(orient='records')
 
 
 @app.post("/api/predict/archive")
 def api_predict_archive():
-    archive = request.files['archive']
-    frames = []
-    if archive and allowed_archive(archive.filename):
-        td = tempfile.TemporaryDirectory()
+    try:
+        archive = request.files['archive']
+    except KeyError:
+        abort(400, description="A file named 'archive' was not found in "
+                               "the request")
+    email_objs = {}
+    if archive and Path(archive.filename).suffix in ALLOWED_ARCHIVES:
         zip_ref = ZipFile(archive)
-        zip_ref.extractall(td.name)
-        for filename in os.scandir(td.name):
-            with open(filename.path, 'rb') as eml:
-                email_obj = email.message_from_binary_file(eml, policy=default)
-                frames.append(
-                    handle_request(email_obj, "file", return_prob=True)
-                    .to_dict(orient='records')
-                ) # in future, pass list to handlerequests and receive 1 df
-    return(frames)
-
-
-def handle_request(email_input, model_type, return_prob=True, 
-                   return_inputs=False):
-    if model_type == "text":
-        model = text_model
-        response_df = pd.DataFrame(
-            {"pred": model.predict(email_input).astype(bool)})
-        if return_prob:
-            response_df['spam_probability'] = \
-                model.predict_proba(email_input)[:,1]
-    elif model_type == "file":
-        model = object_model
-        response_df = pd.DataFrame(
-            {"pred": model.predict([email_input,]).astype(bool)})
-        if return_prob:
-            response_df['spam_probability'] = \
-                model.predict_proba([email_input,])[:,1]
-    if return_inputs and model_type == "text":
-        response_df['subject_head'] = (email_input['subject']
-                                       .str.slice(stop=100))
-        response_df['body_head'] = email_input['body'].str.slice(stop=100)
-    return response_df
-
-
-def retrieve_probas(e, input_type):
-    if input_type == "text":
-        probs = text_model.predict_proba(e)[0]
-        output_str = (f"Probabilities: {probs[0]*100:.2f}% likely to be ham "
-            f"and {probs[1]*100:.2f}% likely to be spam.")
-    elif input_type == "file":
-        email_obj = email.message_from_binary_file(e, policy=default)
-        probs = object_model.predict_proba([email_obj,])[0]
-        output_str = (f"Probabilities: {probs[0]*100:.2f}% likely to be ham "
-            f"and {probs[1]*100:.2f}% likely to be spam.")
-    return output_str
+    else:
+        abort(400, description="'archive' must be one of the following file "
+                                f"types: {', '.join(ALLOWED_ARCHIVES)}")
+    with tempfile.TemporaryDirectory() as td:
+        zip_ref.extractall(td)
+        for file in Path(td).iterdir():
+            if file.suffix not in ALLOWED_EXTENSIONS:
+                abort(400, description="Only email files of the following "
+                                       "types are allowed: "
+                                       f"{', '.join(ALLOWED_EXTENSIONS)}")
+            email_objs[file.name] = email.message_from_bytes(
+                file.read_bytes(),policy=default)
+    email_obj_series = pd.Series(email_objs)
+    response_df = pd.DataFrame({
+            "file": email_obj_series.index,
+            "prediction": OBJECT_MODEL.predict(email_obj_series).astype(bool),
+            "spam_probability": OBJECT_MODEL.predict_proba(email_obj_series)[:,1]
+        })
+    return response_df.to_dict(orient='records')
